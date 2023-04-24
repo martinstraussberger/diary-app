12c16e0177dc02dbc5e85638c236b9bde1444d1e
unknown <martin.straussberger@proton.com>
2023-04-24 12:23:15 +0200

    remove forwarRef

--- src/shared/components/FormElements/TagsInput.tsx
+++ src/shared/components/FormElements/TagsInput.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 interface TagInputProps {
   onChange: (tags: string[]) => void;
@@ -11,94 +11,92 @@ interface TagInputState {
   inputValue: string;
 }
 
-export const TagInput = forwardRef<HTMLInputElement, TagInputProps>(
-  ({ onChange, value, reset }, ref) => {
-    const [state, setState] = useState<TagInputState>({
-      tags: value,
-      inputValue: '',
-    });
+export const TagInput = ({ onChange, value, reset }: TagInputProps) => {
+  const [state, setState] = useState<TagInputState>({
+    tags: value,
+    inputValue: '',
+  });
 
-    const handleInputKeyUp = useCallback(
-      (event: React.KeyboardEvent<HTMLInputElement>) => {
-        const newTag = state.inputValue.trim();
+  const handleInputKeyUp = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const newTag = state.inputValue.trim();
 
-        if (event.code === 'Space') {
-          event.preventDefault();
-          if (newTag !== '' && !state.tags.includes(newTag)) {
-            const newTags = [...state.tags, newTag];
-            setState({
-              tags: newTags,
-              inputValue: '',
-            });
-            onChange(newTags);
-          }
-        } else if (event.key === 'Backspace' && state.inputValue === '') {
-          const newTags = state.tags.slice(0, -1);
+      if (event.code === 'Space') {
+        event.preventDefault();
+        if (newTag !== '' && !state.tags.includes(newTag)) {
+          const newTags = [...state.tags, newTag];
           setState({
             tags: newTags,
             inputValue: '',
           });
           onChange(newTags);
         }
-      },
-      [onChange, state]
-    );
-
-    const handleInputChange = useCallback(
-      (event: React.ChangeEvent<HTMLInputElement>) => {
+      } else if (event.key === 'Backspace' && state.inputValue === '') {
+        const newTags = state.tags.slice(0, -1);
         setState({
-          ...state,
-          inputValue: event.target.value,
-        });
-      },
-      [state]
-    );
-
-    useEffect(() => {
-      if (reset) {
-        setState({
-          tags: [],
+          tags: newTags,
           inputValue: '',
         });
-        onChange([]);
+        onChange(newTags);
       }
-    }, [reset, onChange]);
+    },
+    [onChange, state]
+  );
 
-    return (
-      <>
-        <div className='tag-input'>
-          <div>
-            {state.tags.map((tag, index) => (
-              <span key={index} className='tag-input__tag __field'>
-                {tag}{' '}
-                <button
-                  className='tag-input__tag-remove'
-                  onClick={() => {
-                    const newTags = state.tags.filter((_, i) => i !== index);
-                    setState({
-                      tags: newTags,
-                      inputValue: '',
-                    });
-                    onChange(newTags);
-                  }}
-                >
-                  X
-                </button>
-              </span>
-            ))}
-          </div>
-          <input
-            type='text'
-            className='tag-input__input'
-            onKeyUp={handleInputKeyUp}
-            onChange={handleInputChange}
-            value={state.inputValue}
-          />
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        inputValue: event.target.value,
+      });
+    },
+    [state]
+  );
+
+  useEffect(() => {
+    if (reset) {
+      setState({
+        tags: [],
+        inputValue: '',
+      });
+      onChange([]);
+    }
+  }, [reset, onChange]);
+
+  return (
+    <>
+      <div className='tag-input'>
+        <div>
+          {state.tags.map((tag, index) => (
+            <span key={index} className='tag-input__tag __field'>
+              {tag}{' '}
+              <button
+                className='tag-input__tag-remove'
+                onClick={() => {
+                  const newTags = state.tags.filter((_, i) => i !== index);
+                  setState({
+                    tags: newTags,
+                    inputValue: '',
+                  });
+                  onChange(newTags);
+                }}
+              >
+                X
+              </button>
+            </span>
+          ))}
         </div>
-        <label className='tag-info' htmlFor='tag-input'>
-          Press space to add a new tag
-        </label>
-      </>
-    );
-  }
-);
+        <input
+          type='text'
+          className='tag-input__input'
+          onKeyUp={handleInputKeyUp}
+          onChange={handleInputChange}
+          value={state.inputValue}
+        />
+      </div>
+      <label className='tag-info' htmlFor='tag-input'>
+        Press space to add a new tag
+      </label>
+    </>
+  );
+};
